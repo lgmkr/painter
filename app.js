262de99d12dc2c30cc9d2350fe232dfee320d094
Oleg Makarov <gstancox@gmail.com>
2012-10-23 00:12:54 +0300

    init commit

---
+++ app.js
@@ -0,0 +1,54 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , user = require('./routes/user')
+  , http = require('http')
+  , path = require('path');
+
+var app = express()
+  , server = http.createServer(app)
+  , io = require('socket.io').listen(server);
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+app.get('/', routes.index);
+//app.get('/users', user.list);
+
+server.listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
+
+//var io = require('socket.io').listen(app)
+
+io.sockets.on('connection', function(socket){
+  console.log("Socket connected");
+  socket.on('drawClick', function(data) {
+    socket.broadcast.emit('draw', {
+      x: data.x,
+      y: data.y,
+      type: data.type
+    });
+  });  
+});
+
+
+
+
---
+++ public/javascripts/main.js
@@ -0,0 +1,93 @@
+function ie_event(e)
+{
+    if (e === undefined)
+        { return window.event; };
+    return e;
+}
+
+var Canva = {};
+ 
+// Инициализация объекта
+Canva.init = function(id, width, height)
+{
+  var canv = document.getElementById(id);
+  canv.width = width;
+  canv.height = height;
+ 
+  this.canvasId = id;
+  this.ctx = canv.getContext("2d");
+  this.selectedColor = '#000000';
+  this.selectedFillColor = '#FFFFFF';
+  this.selectedWidth = 1;
+  this.tool = Pencil; // Выбранный инструмент
+  this.drawing = false; // true - если зажата кнопка мыши
+
+  Canva.socket = io.connect('http://localhost:3000')
+  console.log(Canva.socket);
+  Canva.socket.on('draw', function(data) {
+    console.log("inside Canva.draw");
+    return Canva.draw(data.x, data.y, data.type);
+  });
+
+  Canva.draw = function(x, y, type, event){
+    if(type == 'onmousedown'){
+//      var evnt = ie_event(event);
+      Canva.tool.start({clientX:x, clientY:y});
+
+    } else if( type == 'onmouseup'){
+        if (Canva.drawing)
+        {
+//            var evnt = ie_event(event);
+            Canva.tool.finish({clientX:x, clientY:y});
+        }
+    } else if( type == 'onmousemove'){
+        if (Canva.drawing)
+        {
+//            var evnt = ie_event(event);
+            Canva.tool.move({clientX:x, clientY:y});
+        }
+    }  
+  }
+
+    // Кнопка мыши зажата, рисуем
+    canv.onmousedown = function(e)
+    {
+        Canva.draw(e.clientX, e.clientY, "onmousedown");
+        Canva.socket.emit('drawClick', {x: e.clientX, y: e.clientY, type: "onmousedown"});
+    };
+ 
+    // Кнопка мыши отпущена, рисование прекращаем
+    canv.onmouseup = function(e)
+    {
+      Canva.draw(e.clientX, e.clientY, "onmouseup");
+      Canva.socket.emit('drawClick', {x: e.clientX, y: e.clientY, type: "onmouseup"});
+    };
+ 
+    // процесс рисования
+    canv.onmousemove = function(e)
+    {
+      Canva.draw(e.clientX, e.clientY, "onmousemove");
+      Canva.socket.emit('drawClick', {x: e.clientX, y: e.clientY, type: "onmousemove"});
+     };
+};
+ 
+Canva.setTool = function(t) // Задать инструмент
+{
+    Canva.tool = t;
+};
+ 
+Canva.setWidth = function(width) // Задать толщину линий
+{
+    Canvas.selectedWidth = width;
+};
+ 
+Canva.setColor = function(color) // Задать текущий цвет
+{
+    Canva.selectedColor = color;
+};
+ 
+Canva.clear = function() // Очистить рисовалку
+{
+    var canv = document.getElementById(Canva.canvasId);
+    Canva.ctx.clearRect(0, 0, canvas.width, canvas.height);
+};
